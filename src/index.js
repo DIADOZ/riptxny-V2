73ab832475227f47edb14c13a9186e24cc7cbdd9
Mauro Doza <state2good@gmail.com>
2022-09-11 23:43:21 -0400

    added iframes dynamically

--- src/index.js
+++ src/index.js
@@ -19,22 +19,67 @@ playing an opening set for King Woman at The Music Hall of Williamsburg in New
 York. Felipe now resides in NYC, active in a mesh of different cultural 
 communities through his music and pursuing his personal artistic career.`;
 
-createElement('p', {text: bio});
-createElement('h1', {text: 'RIP TXNY'});
-createElement('img', {src: djImage});
-createElement('img', {src: coverImage});
-createElement('img', {src: portalImage});
-createElement('img', {src: qtttcImage});
+createElement('p', {
+	text: bio,
+});
+createElement('h1', {
+	text: 'RIP TXNY',
+});
+createElement('img', {
+	title: 'RIPTXNY on stage',
+	src: djImage,
+});
+createElement('img', {
+	title: 'Cover for RIPTXNY EP',
+	src: coverImage,
+});
+createElement('img', {
+	title: 'Cover for Portal',
+	src: portalImage,
+});
+createElement('img', {
+	title: 'Cover for QTTTC',
+	src: qtttcImage,
+});
 
-// embeds
-// social icons
-createElement('iframe', {src: 'https://open.spotify.com/embed/track/4MVSpERzPckgj9CfBVMyvd?utm_source=generator'});
+createElement('iframe', {
+	title: 'Spotify player for Portal',
+	class: 'spotify-embed',
+	src: 'https://open.spotify.com/embed/track/4MVSpERzPckgj9CfBVMyvd?utm_source=generator',
+	allow: 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture',
+});
+createElement('iframe', {
+	title: 'Youtube video player for Portal',
+	class: 'youtube-embed',
+	src: 'https://www.youtube.com/embed/Ei6mJ2JIPI4',
+	allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
+});
+
+createElement('iframe', {
+	title: 'Apple music player for Portal',
+	class: 'apple-embed',
+	src: 'https://embed.music.apple.com/us/album/portal/1633092892?i=1633092910',
+	allow: 'autoplay *; encrypted-media *; fullscreen *; clipboard-write',
+	sandbox: 'allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation',
+});
+
+
+createElement('iframe', {
+	class: 'bandcamp-embed',
+	title: 'Bandcamp player for Portal',
+	src: 'https://bandcamp.com/EmbeddedPlayer/track=2210751335/size=large/bgcol=333333/linkcol=e32c14/tracklist=false/artwork=small/transparent=true/',
+});
 
 /**
  * Creates DOM elements
  *
  * @param {string} elementType element type to be created
  * @param {object} options any options to be added to element
+ * @returns {
+ *   HTMLElement|
+ *   HTMLIFrameElement|
+ *   HTMLImageElement
+ * } returns created element
  */
 function createElement(elementType, options) {
 	const element = document.createElement(elementType);
@@ -43,8 +88,24 @@ function createElement(elementType, options) {
 		element.appendChild(text);
 	}
 	if (options.hasOwnProperty('src')) {
-		element.src = options.src;
+		element.setAttribute('src', options.src);
+	}
+	if (options.hasOwnProperty('class')) {
+		element.classList.add(options.class);
+	}
+	if (options.hasOwnProperty('title')) {
+		element.setAttribute('title', options.title);
+	}
+	if (options.hasOwnProperty('allow')) {
+		element.setAttribute('allow', options.allow);
+	}
+	if (options.hasOwnProperty('sandbox')) {
+		element.setAttribute('sandbox', options.sandbox);
+	}
+	if (options.hasOwnProperty('allowfullscreen') && options.allowfullscreen) {
+		element.setAttribute('allowfullscreen', '');
 	}
 	document.body.appendChild(element);
+	return element;
 }
 
